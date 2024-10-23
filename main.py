f526b2d7d3f9a6cfd41be1c2c06053ba77c202ac
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2024-10-23 10:51:04 -0700

    done with quiz

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import turtle
+from turtle import *
+from math import *
 
-# YOUR CODE HERE
+f=forward
+b=backward
+r=right
+l=left
+right_angle=90
 
-turtle.exitonclick()
+def isosceles_right_triangle():
+    f(50)
+    r(right_angle)
+    f(50)
+    r(135)
+    f(71)
+    b(71)
+    l(135)
+
+for i in range(4):
+    isosceles_right_triangle()
+    
+exitonclick()
